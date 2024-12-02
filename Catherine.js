054ade39757abb4cd81077aa8dce24b5f6fb95b3
catherinel04 <145166127+catherinel04@users.noreply.github.com>
2024-12-02 18:05:10 -0500

    Add files via upload

---
+++ Catherine.js
@@ -0,0 +1,41 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('#about', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    delay: 400
+});
+
+ScrollReveal().reveal('#genre', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    delay: 400
+});
+
+ScrollReveal().reveal('#artists', {
+    duration: 2000,
+    distance: '70px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    delay: 400
+});
+
+ScrollReveal().reveal('#artists .images img', {
+    duration: 3000,
+    distance: '30px',
+    origin: 'bottom',
+    interval: 200
+});
+
+ScrollReveal().reveal('#songs', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    delay: 400
+});
+
